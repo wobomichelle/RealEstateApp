6c2468f878fbae3b9dca3124a149d03c8641dde3
Michelle <wobomichelle@gmail.com>
2025-11-25 12:37:49 +0100

    text edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
             transition={{duration: 1.5}}
             whileInView={{opacity:1, y:0}}
             viewport={{once: true}}
-            className='container text-center mx-auto  py-4 p-6 md:px-20 lg:px-32 text-white'>
-                <h2 className='text-5xl sm:tet-6xl md:tet-[82px] inline-block max-w-3xxl font-semibold pt-20'>Explore homes that fit your dreams</h2>
+            className='container text-center mx-auto py-4 p-6 md:px-20 lg:px-32 text-white'>
+                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
                 <div className='space-x-6 mt-16'>
                     <a href="#Projects" className='border text-white px-8 py-3 rounded'>Projects</a>
                     <a href="#Contact" className='bg-blue-500 text-white px-8 py-3 rounded'>Contact Us</a>
